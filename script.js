d80a2c1cb002e84a10f937d53e9d393d992244ce
Adam Hills <adamhills91@gmail.com>
2021-12-21 15:21:04 +0000

    Improved game, reset button works. Included images for buttons

--- script.js
+++ script.js
@@ -1,66 +1,74 @@
 let playerScore = 0, computerScore = 0;
 const buttons = document.querySelectorAll('button');
+const replay = document.getElementById('replay');
+replay.addEventListener('click', reset);
+const playerScoreDisplay = document.getElementById('player-score');
+const computerScoreDisplay = document.getElementById('computer-score');
+const resultDisplay = document.getElementById('result');
+const computerChoiceDisplay = document.getElementById('computer-choice');
 
-for (let j = 0; j < buttons.length; j++) {
-    buttons[j].addEventListener('click', function() {
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function(e) {
         if (computerScore < 5 && playerScore < 5) {
-            const playerScoreDisplay = document.getElementById('player-score');
-            const computerScoreDisplay = document.getElementById('computer-score');
-            const resultDisplay = document.getElementById('result');
-            
-            let result = playRound(buttons[j].id);
-             if (result === 1) {
-             playerScore++;
-                console.log('player score is ' + playerScore);
-                playerScoreDisplay.textContent = playerScore;
-                if (playerScore === 5) {
-                    playerScoreDisplay.style.color = '#2ACE36';
-                    resultDisplay.textContent = "You win!"
-                }
-            } else if (result === 0) {
-                computerScore++
-                console.log('computer score is ' + computerScore);
-                computerScoreDisplay.textContent = computerScore;
-                if (computerScore === 5) {
-                   computerScoreDisplay.style.color = '#2ACE36';
-                   resultDisplay.textContent = "You lose!"
-                }
-            } else {
-                console.log('tie');
-            }
+        let result = playRound(e.target);
+        switch (result) {
+            case 1:
+                resultDisplay.textContent = "Yay!";
+                break;
+            case 0: 
+                resultDisplay.textContent = "Oh dear, oh dear!";
+                break;
+            case -1:
+                resultDisplay.textContent = "Go again..."
+                break;
         }
-    }); 
+        let resultTarget = [result, e.target];
+        buttonGlow(resultTarget);
+        calculateScore(result);
+        }
+    })
 }
 
 function playRound(playerSelection) {
-    const playerChoiceDisplay = document.getElementById('player-choice');
-    const computerChoiceDisplay = document.getElementById('computer-choice');
     const computerSelection = computerPlay();
-    playerChoiceDisplay.textContent = playerSelection[0].toUpperCase() + playerSelection.slice(1);
-    computerChoiceDisplay.textContent = computerSelection;
-
-    if (playerSelection === 'rock') {
+    if (playerSelection.id === 'rock') {
         if (computerSelection === 'Paper') {
+            computerChoiceDisplay.src = "images/paper.jpg";
             return 0;
         }
         else if (computerSelection === 'Scissors') {
+            computerChoiceDisplay.src = "images/scissors.jpg";
             return 1;
-        }
-    } else if (playerSelection === 'paper') {
+        } else {
+            computerChoiceDisplay.src = "images/rock.jpg";
+            return -1;
+        } 
+    
+    } else if (playerSelection.id === 'paper') {
         if (computerSelection === 'Scissors') {
+            computerChoiceDisplay.src = "images/scissors.jpg";
             return 0;
         }
         else if (computerSelection === 'Rock') {
+            computerChoiceDisplay.src = "images/rock.jpg";
             return 1;
-        }
-    }  else if (playerSelection === 'scissors') {
+        } else {
+            computerChoiceDisplay.src = "images/paper.jpg";
+            return -1;
+        } 
+    }  else if (playerSelection.id === 'scissors') {
         if (computerSelection === 'Paper') {
+            computerChoiceDisplay.src = "images/paper.jpg";
             return 1;
         }
         else if (computerSelection === 'Rock') {
+            computerChoiceDisplay.src = "images/rock.jpg";
             return 0;
-        }
-    } 
+        } else {
+            computerChoiceDisplay.src = "images/scissors.jpg";
+            return -1;
+        } 
+}
 }
 
 function computerPlay() {
@@ -72,4 +80,57 @@ function computerPlay() {
     } else if (computerChoice === 2) {
         return 'Scissors';
     }
+}
+
+
+function calculateScore(result) {
+        if (result === 1) {
+            playerScore++;
+            playerScoreDisplay.textContent = playerScore;
+            if (playerScore === 5) {
+                playerScoreDisplay.style.color = '#2ACE36';
+                resultDisplay.style.color = '#2ACE36';
+                resultDisplay.textContent = "You Win!"
+                replay.textContent = "Replay?";
+            }
+        } else if (result === 0) {
+            computerScore++
+            computerScoreDisplay.textContent = computerScore;
+            if (computerScore === 5) {
+                computerScoreDisplay.style.color = '#d62323';
+                resultDisplay.style.color = '#d62323';
+                resultDisplay.textContent = "You Lose!"
+                replay.textContent = "Replay?";
+            }
+        }
+}
+
+function buttonGlow(resultTarget) {
+        for (let j = 0; j < buttons.length; j++) {
+            buttons[j].classList.remove('button-win', 'button-tie', 'button-loss');   
+        }
+        if (resultTarget[0] === 1) {
+            resultTarget[1].classList.add('button-win');
+        } else if (resultTarget[0] === 0) {
+            resultTarget[1].classList.add('button-loss');
+        } else {
+            resultTarget[1].classList.add('button-tie');
+        
+    }
+}
+
+function reset() {
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreDisplay.textContent = playerScore;
+    computerScoreDisplay.textContent = playerScore;
+    computerScoreDisplay.style.color = 'black';
+    playerScoreDisplay.style.color = 'black';
+    for (let j = 0; j < buttons.length; j++) {
+        buttons[j].classList.remove('button-win', 'button-tie', 'button-loss');   
+    }
+    computerChoiceDisplay.src = "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=";
+    resultDisplay.textContent = "";
+    resultDisplay.style.color = 'black';
+    replay.textContent = "";
 }
